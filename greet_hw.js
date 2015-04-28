a182ba1e62286dfe2d120af78497263a92a41b7f
Emre Surmeli <s.emre.s.8@gmail.com>
2015-04-28 11:25:43 -0700

    hw1 w/CLI

--- greet_hw.js
+++ greet_hw.js
@@ -1,5 +1,14 @@
 'use strict';
 
-module.exports = function (name) {
-	return "hello" + name;
+var greet = exports = module.exports = function() {};
+
+greet.greeting = function (name) {
+	return "hello " + name;
 };
+
+if(process.argv.length > 2) {
+  for (var i = 2; i < process.argv.length; i++){
+    var insertName = process.argv[i];
+    console.log(greet.greeting(insertName));
+  };
+}
--- test/greet_hw.js
+++ test/greet_hw.js
@@ -1,10 +1,10 @@
 'use strict';
 
 var expect = require('chai').expect;
-var greet = require('../greet_hw.js');
+var greet = require('../greet_hw');
 
 describe('greet_hw.js', function() {
   it('should greet someone, man', function() {
-    expect(greet(' emre')).to.eql('hello emre');
+    expect(greet.greeting('emre')).to.eql('hello emre');
   });
 });
